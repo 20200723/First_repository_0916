0f1369a80bc9dde8f14e53c2d3e346f9647b162f
20200723 <alstlr0718@gmail.com>
2022-09-21 17:08:11 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,12 @@ def print_hello():
 def print_KIT():
     print("Kumoh National Institute of Technology")
 
+def print_Git():
+    print("gitgit hubhub githubgithub githubgit깃깃허브허브깃허브깃허브깃깃허브허브깃허브깃")
 
 if __name__ == '__main__':
     print_hello()
     print_KIT()
+    print_Git()
+
+    
