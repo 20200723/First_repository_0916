61e31d13bf6862b816535b134b2b2ce7d8f579b0
20200723 <alstlr0718@gmail.com>
2022-09-21 17:33:01 +0900

    애드삭제

--- hello_world.py
+++ hello_world.py
@@ -2,11 +2,9 @@ def print_hello():
     print("Hello World!!")
 def print_KIT():
     print("Kumoh National Institute of Technology")
-def add(x,y):
-    a=x+y
+
     return a
 if __name__ == '__main__':
     print_hello()
     print_KIT()
-    print(add("happy","Christmas"))
     
