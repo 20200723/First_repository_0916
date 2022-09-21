35546900358dd63836a1a9f021f5ae059e25b26e
20200723 <alstlr0718@gmail.com>
2022-09-21 17:34:55 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,11 @@ def print_hello():
     print("Hello World!!")
 def print_KIT():
     print("Kumoh National Institute of Technology")
-
+def add(x,y):
+    a=x+y+"hihi"
     return a
 if __name__ == '__main__':
     print_hello()
     print_KIT()
+    print(add("happy","Christmas"))
     
